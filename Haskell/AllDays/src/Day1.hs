03575b0ba0805d6fad58afa22183f1263d1374bb
Mike Houghton <mike_k_houghton@yahoo.co.uk>
2020-12-01 18:54:34 +0000

    Tidy.

--- Haskell/AllDays/src/Day1.hs
+++ Haskell/AllDays/src/Day1.hs
@@ -2,7 +2,7 @@ module Day1 where
 
 
 intList :: [String] -> [Int]
-intList  = map (\x -> read x :: Int) 
+intList = map (\ x -> read x :: Int)
 
 sumIs2020P1 :: [Int] -> Int
 sumIs2020P1 xs = a * b  where (a, b):hs =  [(x,y) | x <- xs, y <- xs,  x + y == 2020]
@@ -12,7 +12,7 @@ sumIs2020P2 xs = a * b * c where (a, b, c):hs =  [(x,y,z) | x <- xs, y <- xs, z 
 
 day1Main :: IO () 
 day1Main = do
-    content <- readFile ("data/Day1Part1.txt")
+    content <- readFile "data/Day1Part1.txt"
     let p1 = sumIs2020P1 . intList . lines $ content 
     print p1
     let p2 = sumIs2020P2 . intList . lines $ content 

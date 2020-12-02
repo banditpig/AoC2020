da77d2156bbfc64a3b09f102ebd4ae2e826a5700
Mike Houghton <mike_k_houghton@yahoo.co.uk>
2020-12-02 12:21:20 +0000

    Day2 part 2

--- Kotlin/src/main/kotlin/Day2.kt
+++ Kotlin/src/main/kotlin/Day2.kt
@@ -10,7 +10,7 @@ data class Criteria(private val criteriaPasswd: String){
        val (criteria, pwd) = Pair(
                criteriaPasswd.split(":")[0],
                criteriaPasswd.split(":")[1])
-       this.password = pwd
+       this.password = pwd.trim()
 
        //14-15 d
        val halves = criteria.split(' ')
@@ -24,25 +24,44 @@ data class Criteria(private val criteriaPasswd: String){
                                .toInt())
    }
 }
-fun validate(cr: Criteria): Boolean{
-    val letterCount = cr.password.filter {it == cr.letter }
-                                 .count()
+fun validate1(cr: Criteria): Boolean{
+    val letterCount = cr.password
+            .filter {it == cr.letter }
+            .count()
+
     return  letterCount >= cr.minMax.first
             &&
             letterCount <= cr.minMax.second
 
 }
+fun validate2(cr: Criteria): Boolean{
 
+   // letter in min or max not both
+    val ch1 = cr.password[cr.minMax.first - 1]
+    val ch2 = cr.password[cr.minMax.second - 1]
+    return ((ch1 == cr.letter && ch2 != cr.letter)
+            ||
+            (ch2 == cr.letter && ch1 != cr.letter))
+}
 
 
 fun part1(): Int{
+
     return readFileAsLines("resources/Day2Part1.txt")
             .map { Criteria(it) }
-            .filter{validate(it)}
+            .filter{validate1(it)}
+            .count()
+}
+fun part2(): Int{
+
+    return readFileAsLines("resources/Day2Part1.txt")
+            .map { Criteria(it) }
+            .filter{validate2(it)}
             .count()
 }
 
 fun main(args: Array<String>) {
-   println(part1())
+    println(part1())
+    println(part2())
 
 }

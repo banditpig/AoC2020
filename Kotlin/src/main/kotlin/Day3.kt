d791c9004eeaf20ecd00619a2e2c61f13eeeb952
Mike Houghton <mike_k_houghton@yahoo.co.uk>
2020-12-03 13:10:34 +0000

    Tidy Day3.kt

--- Kotlin/src/main/kotlin/Day3.kt
+++ Kotlin/src/main/kotlin/Day3.kt
@@ -6,6 +6,12 @@ typealias MapCell = Char
 const val tree: MapCell = '#'
 
 typealias Point = Pair<Int, Int>
+val f1 = { xy: Point -> Point(xy.first + 1, xy.second + 1 )}
+val f2 = { xy: Point -> Point(xy.first + 3, xy.second + 1 )}
+val f3 = { xy: Point -> Point(xy.first + 5, xy.second + 1)}
+val f4 = { xy: Point -> Point(xy.first + 7, xy.second + 1 )}
+val f5 = { xy: Point -> Point(xy.first + 1, xy.second + 2)}
+val allFs = listOf(f1, f2, f3, f4, f5)
 
 fun itemAt( xy: Point, tm: TreeMap): Char {
     if (xy.second > tm.size) {
@@ -29,12 +35,6 @@ fun part1(treeMap: List<String>, stepFunc: (Point) -> Point): Int{
             .count()
 }
 fun part2(treeMap: List<String>): Long{
-    val f1 = { xy: Point -> Point(xy.first + 1, xy.second + 1 )}
-    val f2 = { xy: Point -> Point(xy.first + 3, xy.second + 1 )}
-    val f3 = { xy: Point -> Point(xy.first + 5, xy.second + 1)}
-    val f4 = { xy: Point -> Point(xy.first + 7, xy.second + 1 )}
-    val f5 = { xy: Point -> Point(xy.first + 1, xy.second + 2)}
-    val allFs = listOf(f1, f2, f3, f4, f5)
 
     return allFs.map {part1(treeMap, it)}
             .fold(1, {acc, x -> x * acc })
@@ -42,7 +42,6 @@ fun part2(treeMap: List<String>): Long{
 
 fun main(args: Array<String>) {
     val treeMap = readFileAsLines("resources/Day3Part1.txt")
-    val f = { xy: Point -> Point(xy.first + 3, xy.second + 1 )}
-    println(part1(treeMap, f))
+    println(part1(treeMap, f2))
     print(part2(treeMap))
 }

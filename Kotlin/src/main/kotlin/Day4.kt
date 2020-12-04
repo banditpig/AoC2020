79e1c5f73a5423f85921ea1800ae1672fbf82686
mike_k_houghton <mike_k_houghton@yahoo.co.uk>
2020-12-04 17:48:26 +0000

    Day 4 part 2 Kotlin.

--- Kotlin/src/main/kotlin/Day4.kt
+++ Kotlin/src/main/kotlin/Day4.kt
@@ -7,6 +7,47 @@ typealias Passport = List<Pair<Key, Value>>
 
 val  keysNoCid = listOf("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid")
 
+val validators = mapOf(
+    "byr" to  ::birthCheck,
+    "iyr" to  ::issueCheck,
+    "eyr" to  ::expireCheck,
+    "hgt" to  ::heightCheck,
+    "hcl" to  ::hairCheck,
+    "ecl" to  ::eyeCheck,
+    "pid" to  ::pidCheck,
+    "cid" to  { true}
+)
+
+fun numberCheck(lo:Int, hi: Int, num: Int ): Boolean = num in lo..hi
+fun birthCheck(yr: String): Boolean =
+        numberCheck(1920, 2002, yr.toInt())
+fun issueCheck(yr: String): Boolean =
+        numberCheck(2010, 2020, yr.toInt())
+fun expireCheck(yr: String): Boolean =
+        numberCheck(2020, 2030, yr.toInt())
+
+fun hairCheck(hair: String): Boolean =
+        hair.matches(Regex("#[0-9,a-f]{6}"))
+
+fun eyeCheck(eye: String): Boolean =
+        listOf("amb","blu","brn", "gry", "grn", "hzl","oth").contains(eye)
+
+fun pidCheck(pid: String): Boolean =
+        pid.matches(Regex("^[0-9]{9}\$"))
+
+
+fun heightCheck(height: String): Boolean{
+    val post = height.takeLast(2)
+    val numStr = height.dropLastWhile { it.isLetter() }
+    return when(post){
+        "cm" -> numberCheck(150, 193, numStr.toInt())
+        "in" -> numberCheck(59, 76, numStr.toInt())
+        else -> false
+    }
+}
+
+
+
 fun parseLine(line: String):  List<Pair<Key, Value>> =
     line.split(' ')
         .map { Pair(it.split(':')[0], it.split(':')[1]) }
@@ -25,16 +66,24 @@ fun passportHasValidKeys(p: Passport): Boolean{
     } )
     return keyList.containsAll(keysNoCid)
 }
-fun passportHasValidValues(p: Passport) : Boolean{
-    return false;
-}
+fun passportHasValidValues(p: Passport) : Boolean =
+     p.map { kv ->
+        validators.getValue(kv.first).invoke(kv.second)
+    }.filter { it }.count() == p.size
+
+
 
 fun passportsWithValidKeys(passports: List<Passport>):List<Passport> {
-    return passports.fold(mutableListOf<Passport>(), {acc, p ->
+    return passports.fold(mutableListOf(), { acc, p ->
         if(passportHasValidKeys(p)) acc.add(p)
         acc
         })
 }
+fun part2(passports: List<Passport>): Int =
+       passportsWithValidKeys(passports)
+               .map { passportHasValidValues(it) }
+               .filter { it }
+               .size
 
 fun main(args: Array<String>) {
     val data = File("resources/Day4Part1.txt").readText()
@@ -42,5 +91,7 @@ fun main(args: Array<String>) {
     println("Part 1")
     println(passportsWithValidKeys(all).size)
 
+    println("Part 2")
+    println(part2(all))
 
 }

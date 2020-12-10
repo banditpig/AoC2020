e58f3033b750d1ab3c9e9ef6c370ceda365c04e1
Mike Houghton <mike_k_houghton@yahoo.co.uk>
2020-12-10 14:15:00 +0000

    Day 9 part 2. Haskell

--- Haskell/AllDays/src/Day9.hs
+++ Haskell/AllDays/src/Day9.hs
@@ -1,9 +1,27 @@
 module Day9 where
 import Data.List
 import Data.Maybe
-
+import Data.Sort
+import Control.Lens
+import Control.Lens.Indexed
 type Numbers = [Int]
 type Window = Int
+
+sumOverWindow ::[Int] ->  Int -> Int -> (Int, Int, Int)
+sumOverWindow nums  wide lo  = res  where
+    end =   min (lo + wide) (length nums)
+    r1 = map (\x -> nums ^? ix x)  [lo .. end]
+    res = (sum (catMaybes r1), lo, end) 
+ 
+
+
+allWindows 1000 nums target = [(0,0,0)]
+allWindows w nums target 
+  | results  /= [] = results
+  | otherwise = allWindows (w + 1) nums target
+      where 
+        sumw = map (sumOverWindow nums  w)  [0.. length nums] 
+        results = filter (\(v, l, u) -> v == target) sumw
 
 numberOk ::  Numbers -> Window -> Int -> Bool
 numberOk  nums w n 
@@ -20,11 +38,25 @@ part1 nums w = res where
                 . dropWhile fst 
                 . map (\x -> (numberOk nums w x, x )) 
                 . drop w $ nums
-   
+
+
+part2  nums bad = res where 
+  [(_, lo, hi)] = allWindows 2 nums bad
+  range = sort . take (hi - 1) $ drop lo nums
+  res  = head range + last range
+
+part2'  nums bad = range where 
+  [(_, lo, hi)] = allWindows 2 nums bad
+  range = sum . sort . take (hi - 1) $ drop lo nums
 
 day9Main :: IO ()
 day9Main = do 
+    let pre = 25
     inp <- readFile "../data/Day9Part1.txt" 
     let nums = map (\s -> read s::Int) $ lines inp
-    
-    print (part1 nums 25)
+    let bad  = part1 nums pre
+    print bad
+    -- Part 2 works with test data but not with real data. :)
+    print "===="
+    print (part2 nums bad) 
+   

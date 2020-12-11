ee506c1c8b374398fb65c21cdd589305029c9708
Mike Houghton <mike_k_houghton@yahoo.co.uk>
2020-12-11 10:42:00 +0000

    Uses graph structure to compute paths

--- Haskell/AllDays/src/Day10.hs
+++ Haskell/AllDays/src/Day10.hs
@@ -1,10 +1,45 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
+
 {-# OPTIONS -Wall -fno-warn-type-defaults #-}
 {-# OPTIONS -fwarn-typed-holes #-}
-
+-- https://stackoverflow.com/questions/11168238/haskell-generating-all-paths-between-nodes
 module Day10 where
-import Data.Sort
+import  Data.Sort 
+import           Data.IntMap (IntMap)
+import           Data.IntSet (IntSet)
+import qualified Data.IntMap as IntMap
+import qualified Data.IntSet as IntSet
+import qualified Data.Map as M
+
+
+   
+
+buildGraph :: [Int] -> IntMap [Int]
+buildGraph xs = 
+    foldl (\acc x  -> IntMap.insert x (nodeList x xs) acc ) IntMap.empty xs
+    where 
+        nodeList x xs = [ x + j | j <- [1,2,3], (x + j) `elem` xs]
+
+
+connect :: Int -> Int -> IntMap [Int] -> [[(Int, Int)]]
+connect x y g = helper x y g [x]
+  where
+    helper a b g visited
+        | a == b    = [[]]
+        | otherwise = [(a,c):path | c <- neigh a, c `notElem` visited, path <- helper c b g (c:visited)]
+        where
+            neigh i = IntMap.findWithDefault [] i g
+
+
+
+
+diffs :: [Int] -> [Int]
+diffs xs@(_:xxs) = zipWith (-) xxs xs
+
+freqs :: Ord a => [a] -> M.Map a Int
+freqs = M.fromListWith (+) . map (,1) 
 
 next :: Int -> [Int] -> Int
 next v vs
@@ -18,11 +53,9 @@ buildList done  v vs
     where nxtv = next v vs
  
 fixList :: [Int] -> ([Int], Int)
-fixList vs = (0 : myV : svs, myV) where
+fixList vs = (0  : svs ++ [myV], myV) where
     svs = sort vs
     myV = 3 + last svs
-
-
 
 addPairs :: (Int, Int) -> (Int, Int) -> (Int, Int)
 addPairs (a, b) (c, d) = (a + c, b + d)
@@ -42,4 +75,11 @@ day10Main = do
     let  (vs, done) = fixList nums
     let (a,b) = threesOnes $ buildList  done 0 vs 
     print ( a * b )
-    
+   
+    print "part2"
+    -- OK print (paths  (IntSet.fromList  vs))
+    let g = buildGraph vs
+    print g
+    let con = connect 0 22 g
+    print con
+    print (length con)  
